b9f8894bffc1781c79b9b9856092c790070a0b7c
Mohamed Salman <mohamed.akj@gmail.com>
2022-11-27 21:36:10 +0530

    Added complexities for Array DS

--- Data_Structures/Python/arrays.py
+++ Data_Structures/Python/arrays.py
@@ -1,5 +1,26 @@
 # Questions - https://github.com/codebasics/data-structures-algorithms-python/blob/master/data_structures/2_Arrays/2_arrays_exercise.md
 
+"""
+Complexities:
+
+array = [2200, 2350, 2600, 2130, 2190]
+
+1. Look-up by index - Constant O(1)
+    => array[1]
+    
+2. Look-up by Value - Linear O(n)
+    => for i in range(len(array)):
+        if array[i] == 2350:
+            return i
+
+3. Traverse the array (like print all elements) - Linear O(n)
+    => for arr in array:
+        print(arr)
+
+4. Insert to / Remove from an array at a specific index - Linear O(n)
+    => Because, these operations needs to update the following items' position
+"""
+
 expenses = [2200, 2350, 2600, 2130, 2190]
 
 # 1
