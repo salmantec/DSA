a7d67665ecb04efb2e5638cc2464d8cce806104d
Mohamed Salman <mohamed.akj@gmail.com>
2022-11-27 19:49:28 +0530

    Added Array and LinkedList Python DS

---
+++ Data_Structures/Python/arrays.py
@@ -0,0 +1,28 @@
+# Questions - https://github.com/codebasics/data-structures-algorithms-python/blob/master/data_structures/2_Arrays/2_arrays_exercise.md
+
+expenses = [2200, 2350, 2600, 2130, 2190]
+
+# 1
+extraDollarsSpentInFeb = expenses[1] - expenses[0]
+print('Extra dollars spent in Feb month ', extraDollarsSpentInFeb)
+
+# 2
+quarterTotalExpenses = expenses[0] + expenses[1] + expenses[2]
+print('Total expenses of first quarter ', quarterTotalExpenses)
+
+# 3
+for i in range(len(expenses)):
+    if expenses[i] == 2000:
+        hasSpent2000Dollars = True
+    else:
+        hasSpent2000Dollars = False
+
+print('Did we spent exactly 2000 dollars in any of the month ', hasSpent2000Dollars)
+
+# 4
+expenses.append(1980)
+print('Added june month expenses ', expenses)
+
+# 5
+expenses[3] = expenses[3] - 200
+print('Updated april month expenses ', expenses)
